3445ccadef379fc6643725e788401e53c634173e
ManasKush <manaskush311@gmail.com>
2025-07-03 11:32:44 +0530

    error removed

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,17 +2,36 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import {CreateUserSchema,SigninSchema,CreateRoomSchema} from "@repo/common/types"
 
 const app=express();
 
 // Define a route
 app.post('/signup', (req, res) => {
-  //db call
+  
+  const data=CreateUserSchema.safeParse(req.body);
+  if(!data.success){
+    res.json({
+        message:"Incorrect inputs"
+    })
+    return
+  }
+ 
+    //db call
   res.json({
     userId:"123"
   })
 });
 app.post('/signin', (req, res) => {
+
+  
+  const data=SigninSchema.safeParse(req.body);
+  if(!data.success){
+    res.json({
+        message:"Incorrect inputs"
+    })
+    return
+  }   
  const userId=1;
  const token=jwt.sign({
     userId
@@ -23,7 +42,16 @@ app.post('/signin', (req, res) => {
  })
 });
 app.post('/room',middleware, (req, res) => {
-  //db call
+   
+  const data=CreateRoomSchema.safeParse(req.body);
+  if(!data.success){
+    res.json({
+        message:"Incorrect inputs"
+    })
+    return
+  }
+  
+    //db call
 
   res.json({
     roomId:123
