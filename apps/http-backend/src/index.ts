d845763f0e58f47710aabd3601fe198317cf057e
ManasKush <manaskush311@gmail.com>
2025-07-03 12:59:52 +0530

    added signup ep

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,19 +3,36 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
 import {CreateUserSchema,SigninSchema,CreateRoomSchema} from "@repo/common/types"
+import { prismaClient } from "@repo/db/client";
+
 
 const app=express();
 
 // Define a route
-app.post('/signup', (req, res) => {
+app.post('/signup', async (req, res) => {
   
-  const data=CreateUserSchema.safeParse(req.body);
-  if(!data.success){
+  const parsedData=CreateUserSchema.safeParse(req.body);
+  if(!parsedData.success){
     res.json({
         message:"Incorrect inputs"
     })
     return
   }
+  try {
+  await prismaClient.user.create({
+    data:{
+email:parsedData.data?.username,
+password:parsedData.data.password,
+name:parsedData.data.name,
+
+    }
+  })  
+  } catch (e) {
+    res.status(411).json({
+        message:"User already exists with this username"
+    })
+  }
+  
  
     //db call
   res.json({
