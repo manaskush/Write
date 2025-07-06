f58350ac0ceb6269ae1478d334324caad1ffbc0a
ManasKush <manaskush311@gmail.com>
2025-07-06 14:44:39 +0530

    Canvas Building

---
+++ apps/draw-front-end/app/canvas/[roomId]/page.tsx
@@ -0,0 +1,23 @@
+import { useEffect,useRef } from "react";
+
+export default function Canvas(){
+    const canvasRef=useRef<HTMLCanvasElement>(null);
+
+    useEffect(()=>{
+
+        if(canvasRef.current){
+            const canvas=canvasRef.current;
+            const ctx=canvas.getContext("2d");
+            
+          if(!ctx){
+            return;
+          }
+             ctx.strokeRect(0,0,canvas.width,canvas.height);
+
+            // Add more drawing logic here
+        }
+    },[canvasRef]);
+    return<div>
+        <canvas ref={canvasRef} width={500} height={500}></canvas>
+    </div>
+}
